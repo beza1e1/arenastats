916aeb03972501a33a1cee5d5d4556afe6176bb8
Andreas Zwinkau <zwinkau@kit.edu>
2010-03-19 14:01:10 +0100

    Extract player profile output to extra file

--- quakelog/nicklog.py
+++ quakelog/nicklog.py
@@ -54,32 +54,3 @@ def merge_player_lines(lines):
 			merge(player, players[player.nick])
 	for nick, player in players.items():
 		yield player
-
-_ODD_CLASS = {True: 'odd', False: 'even'}
-_HTML= """\
-<html>
-<head>
-	<title>%s</title>
-	<style>
-	tr.odd { background-color: #ddd; }
-	th { font-weight: normal; text-align: left; }
-	</style>
-</head>
-<body>
-	<h1>%s profile</h1>
-	%s
-</body>
-</html>
-"""
-def player_profile(player):
-	html = ""
-	html += "<table>"
-	odd = False
-	for prop in _ZERO_PROPERTIES:
-		html += '<tr class="%s"><th>%s</th><td>%d</td></tr>' % (_ODD_CLASS[odd], prop, getattr(player, prop))
-		odd = not odd
-	for weapon in _STAT_WEAPONS.values():
-		for key, val in getattr(player, weapon).items():
-			html += '<tr class="%s"><th>%s %s</th><td>%d</td></tr>' % (_ODD_CLASS[odd], weapon, key, val)
-	html += "</table>\n"
-	return _HTML % (player.nick, player.nick, html)
